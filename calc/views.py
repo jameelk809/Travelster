8c661ef86a44e7c88f8399976f5c08feb2cfa74c
Kamran <jameelk809@gmail.com>
2022-04-20 21:19:24 +0530

    add two numbers
    GET method

--- calc/views.py
+++ calc/views.py
@@ -5,3 +5,10 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {'name': 'Kamran'})
+
+
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1 + val2
+    return render(request, "result.html", {'result': res})
