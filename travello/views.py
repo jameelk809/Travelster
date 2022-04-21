bc6352aa4be7e4b38b0d21781124bb90a32504d1
Kamran <jameelk809@gmail.com>
2022-04-21 22:47:06 +0530

    passing dynamic data, Part: 2

--- travello/views.py
+++ travello/views.py
@@ -9,4 +9,20 @@ def index(request):
     dest1.name = 'Mumbai'
     dest1.desc = 'The city that never sleeps'
     dest1.price = 700
-    return render(request, 'index.html', {'dest1': dest1})
+    dest1.img = 'destination_1.jpg'
+
+    dest2 = Destination()
+    dest2.name = 'Hyderabad'
+    dest2.desc = 'First Biryani, then Sherwani'
+    dest2.price = 650
+    dest2.img = 'destination_2.jpg'
+
+    dest3 = Destination()
+    dest3.name = 'Bangalore'
+    dest3.desc = 'Beautiful city'
+    dest3.price = 600
+    dest3.img = 'destination_3.jpg'
+
+    dests = [dest1, dest2, dest3]
+
+    return render(request, 'index.html', {'dests': dests})
