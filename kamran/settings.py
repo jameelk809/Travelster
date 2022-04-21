b15f89bd66df0f877ec114b92214980e1ca1e5a3
Kamran <jameelk809@gmail.com>
2022-04-22 01:36:12 +0530

    superuser created, destinations added from database
    >> python manage.py createsuperuser

--- kamran/settings.py
+++ kamran/settings.py
@@ -128,3 +128,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
     ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
