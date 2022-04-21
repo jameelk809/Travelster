eac67ea146affc00d91ec5fec60c9d5a0b6fd34c
Kamran <jameelk809@gmail.com>
2022-04-21 23:03:52 +0530

    if condition on Offers implemented

--- travello/views.py
+++ travello/views.py
@@ -10,18 +10,21 @@ def index(request):
     dest1.desc = 'The city that never sleeps'
     dest1.price = 700
     dest1.img = 'destination_1.jpg'
+    dest1.offer = False
 
     dest2 = Destination()
     dest2.name = 'Hyderabad'
     dest2.desc = 'First Biryani, then Sherwani'
     dest2.price = 650
     dest2.img = 'destination_2.jpg'
+    dest2.offer = True
 
     dest3 = Destination()
     dest3.name = 'Bangalore'
     dest3.desc = 'Beautiful city'
     dest3.price = 600
     dest3.img = 'destination_3.jpg'
+    dest3.offer = False
 
     dests = [dest1, dest2, dest3]
 
