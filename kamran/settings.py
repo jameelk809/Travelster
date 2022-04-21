bf7662166cb329d4e222446a99fcfffaf65e4337
Kamran <jameelk809@gmail.com>
2022-04-22 00:53:33 +0530

    migrations
    >> pip install Pillow
    >> python manage.py makemigrations
    >> python manage.py sqlmigrate travello 0001
    >> python manage.py migrate

--- kamran/settings.py
+++ kamran/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'kamran.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
     }
 }
 
