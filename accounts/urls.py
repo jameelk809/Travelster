105be34e38f945e1d68dd14303b4fd88c58ac7e3
Kamran <jameelk809@gmail.com>
2022-04-23 00:12:17 +0530

    user login

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('register', views.register, name = 'register'),
+    path("register", views.register, name="register"),
+    path("login",views.login, name="login"),
 
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,26 @@
-from pyexpat.errors import messages
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 
 # Create your views here.
+
+
+def login(request):
+    if request.method== 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username,password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request,'invalid credentials')
+            return redirect('login')
+
+    else:
+        return render(request,'login.html')    
 
 def register(request):
 
@@ -10,24 +28,37 @@ def register(request):
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         username = request.POST['username']
-        email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
-        if password1 == password2:
+        email = request.POST['email']
+
+        if password1==password2:
             if User.objects.filter(username=username).exists():
-                messages.info(request, 'Username Taken')
+                messages.info(request,'Username Taken')
                 return redirect('register')
             elif User.objects.filter(email=email).exists():
-                messages.info(request, 'Email Taken')
+                messages.info(request,'Email Taken')
                 return redirect('register')
-            else:
+            else:   
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
-                user.save()
-                print('User created')
+                user.save();
+                print('user created')
+                return redirect('login')
+
         else:
-            messages.info(request, 'Password does not match')
+            messages.info(request,'password not matching..')    
             return redirect('register')
         return redirect('/')
+        
     else:
-        return render(request, 'register.html')
+        return render(request,'register.html')
 
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')       
+
+
+def contact(request):
+    return render(request,'contact.html')
