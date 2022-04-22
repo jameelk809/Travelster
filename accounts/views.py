634214a5f99f4ac55cf7ce07e972b8db4377dfd3
Kamran <jameelk809@gmail.com>
2022-04-22 20:27:14 +0530

    User Registration: 2

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 
 def register(request):
-    return render(request, 'register.html')
+
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                print('Username already exists')
+            elif User.objects.filter(email=email).exists():
+                print('Email already exists')
+            else:
+                user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                print('User created')
+        else:
+            print('Password does not match')
+        return redirect('/')
+    else:
+        return render(request, 'register.html')
 
