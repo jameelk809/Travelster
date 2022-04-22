4c125cad6a04a0eec997994f9320ffd6706ee9b2
Kamran <jameelk809@gmail.com>
2022-04-22 23:16:32 +0530

    passing messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from pyexpat.errors import messages
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User, auth
 
@@ -14,15 +15,18 @@ def register(request):
         password2 = request.POST['password2']
         if password1 == password2:
             if User.objects.filter(username=username).exists():
-                print('Username already exists')
+                messages.info(request, 'Username Taken')
+                return redirect('register')
             elif User.objects.filter(email=email).exists():
-                print('Email already exists')
+                messages.info(request, 'Email Taken')
+                return redirect('register')
             else:
                 user = User.objects.create_user(username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
                 user.save()
                 print('User created')
         else:
-            print('Password does not match')
+            messages.info(request, 'Password does not match')
+            return redirect('register')
         return redirect('/')
     else:
         return render(request, 'register.html')
